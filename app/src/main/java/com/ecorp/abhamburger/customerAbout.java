4339a481eaa98cfd34ab9b8939b38dea804c7eab
Ben Horn <benhorenn@gmail.com>
2018-12-17 20:58:00 +0200

    fix bugs

--- app/src/main/java/com/ecorp/abhamburger/customerAbout.java
+++ app/src/main/java/com/ecorp/abhamburger/customerAbout.java
@@ -1,7 +1,6 @@
 package com.ecorp.abhamburger;
-
 import android.os.Bundle;
-        import android.support.v4.app.Fragment;
+import android.support.v4.app.Fragment;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
