cf44b1f21072c27544e97d93aa0caf1bc4c3a05b
ofir68681 <ofir68681@gmail.com>
2018-12-23 12:35:20 +0200

    ben is candy

--- app/src/main/java/com/ecorp/abhamburger/Employee.java
+++ app/src/main/java/com/ecorp/abhamburger/Employee.java
@@ -6,16 +6,13 @@ import java.util.List;
 
 public class Employee extends Person {
 
-    static int idGenerator = 1111;
 
-    int emoplyeeID;
     double salary = 4000;
     String type = "seller";
     List<Integer> orderID;
 
     public Employee(String fName, String lName, String email,String password) {
         super(fName, lName, email, password);
-        this.emoplyeeID = idGenerator ++;
         this.orderID = new ArrayList<Integer>();
     }
 }
--- app/src/main/java/com/ecorp/abhamburger/Manager.java
+++ app/src/main/java/com/ecorp/abhamburger/Manager.java
@@ -3,12 +3,9 @@ package com.ecorp.abhamburger;
 import java.util.Date;
 
 public class Manager extends Person{
-    int id;
-    static int idGenerator = 1;
 
     public Manager(String firstName, String lastName, String email, String password) {
         super(firstName, lastName, email, password);
-        id = idGenerator++;
     }
 
 
